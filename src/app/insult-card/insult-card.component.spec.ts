7b34a79164cf31cfb0663944e466c948ceca3d63
craigles <craig.d.curtis@gmail.com>
2017-05-28 12:25:01 +1000

    import router

--- src/app/insult-card/insult-card.component.spec.ts
+++ src/app/insult-card/insult-card.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Router } from '@angular/router';
 import { InsultCardComponent } from './insult-card.component';
 
 describe('InsultCardComponent', () => {
