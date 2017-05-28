5d4330e604a4a21887f728076b1833e2c9d18dc3
craigles <craig.d.curtis@gmail.com>
2017-05-28 12:31:47 +1000

    import routermodule

--- src/app/insult-form/insult-form.component.spec.ts
+++ src/app/insult-form/insult-form.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule  } from '@angular/router/testing';
+import { RouterModule } from '@angular/router';
 import { InsultFormComponent } from './insult-form.component';
 
 describe('InsultFormComponent', () => {
